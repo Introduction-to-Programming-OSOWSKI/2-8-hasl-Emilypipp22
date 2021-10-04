67c82e0386d72e88c89762d18f9cf5da138d0741
Emilypipp22 <814803@mystma.org>
2021-10-04 09:40:30 -0500

    first try

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+#define function
+def hasL(w):
+    for i in range(0, len(w)):
+        if w[i] == "l":
+            return True
+    return False
+print(hasL("lovely"))
